6699939b56a4dc0ba9554b9127ec8e1b4576d917
einerzg <einerzg@gmail.com>
2024-03-11 11:41:19 -0500

    Setup using npm

---
+++ project-leaflet/main.js
@@ -0,0 +1,8 @@
+import L from "leaflet";
+
+const map = L.map('map').setView([4.53389, -75.68111], 13);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+	maxZoom: 19,
+	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
