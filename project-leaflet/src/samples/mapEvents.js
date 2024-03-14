7450cf57b35139ee7922c0340509e5e5b6db47b8
einerzg <einerzg@gmail.com>
2024-03-14 15:54:23 -0500

    Add example for map events

---
+++ project-leaflet/src/samples/mapEvents.js
@@ -0,0 +1,44 @@
+import { Map, marker } from "leaflet";
+import { createTileLayers } from "../helper";
+import {
+  DEFAULT_ZOOM,
+  LAT_UNIQUINDIO,
+  LNG_UNIQUINDIO,
+} from "../helper/constants";
+
+const map = new Map("map", {
+    doubleClickZoom: false,
+}).setView(
+  [LAT_UNIQUINDIO, LNG_UNIQUINDIO],
+  DEFAULT_ZOOM
+);
+
+createTileLayers().addTo(map);
+
+map.on("click", function (eventData) {
+  console.log("Clicking", eventData);
+  marker(eventData.latlng).addTo(map).bindPopup(eventData.latlng.toString())
+});
+
+// don't working
+map.on("dbclick", function (eventData) {
+  console.log("Double click", eventData);
+});
+
+map.on("contextmenu", function (eventData) {
+  console.log("Rigth click", eventData);
+});
+
+map.on("keypress", function (eventData) {
+  console.log("Keypress", eventData);
+});
+
+map.on("locationerror", function(eventData) {
+    console.log("locationerror", eventData);
+});
+
+map.on("locationfound", function(eventData) {
+    console.log("locationfound", eventData);
+});
+
+map.locate();
