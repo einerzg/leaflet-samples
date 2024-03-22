d5b8c26f7954c4a5b8833ec0c84af65a0d25f3c4
einerzg <einerzg@gmail.com>
2024-03-22 15:09:34 -0500

    Add layer group example

--- project-leaflet/src/samples/controlLayers/controLayers.js
+++ project-leaflet/src/samples/controlLayers/controLayers.js
@@ -1,9 +1,13 @@
-import { Map, control, polyline } from 'leaflet';
+import { Map, control, layerGroup, marker, polyline } from 'leaflet';
 import { createTileLayers } from '../../helper';
 import {
   CALARCA,
   DEFAULT_ZOOM,
+  LAT_ARMENIA,
+  LAT_HOSPITAL,
   LAT_UNIQUINDIO,
+  LNG_ARMENIA,
+  LNG_HOSPITAL,
   LNG_UNIQUINDIO,
   MY_HOME
 } from '../../helper/constants';
@@ -20,8 +24,19 @@ const defaultLayer = createTileLayers(
 const secondLayer = createTileLayers(layers.baseLayers.osmManik.map);
 
 // Overlayers
+const markersLayer = layerGroup();
+const markers = [
+  marker([LAT_ARMENIA, LNG_ARMENIA]).bindPopup('ARMENIA'),
+  marker([LAT_UNIQUINDIO, LNG_UNIQUINDIO]).bindPopup('UNIQUINDIO'),
+  marker([LAT_HOSPITAL, LNG_HOSPITAL]).bindPopup('HOSPITAL')
+];
+
+markers.forEach(function (marker) {
+  markersLayer.addLayer(marker);
+});
+
 const sites = [MY_HOME, CALARCA];
-const mySites = polyline(sites, { color: 'red' }).addTo(map);
+const mySites = polyline(sites, { color: 'red' });
 
 map.fitBounds(sites);
 
@@ -31,7 +46,8 @@ const baseLayers = {
 };
 
 const overlays = {
-  Marker: mySites
+  Polynes: mySites,
+  Marker: markersLayer
 };
 
-control.layers(baseLayers, overlays, { collapsed: false }).addTo(map);
+control.layers(baseLayers, overlays).addTo(map);
