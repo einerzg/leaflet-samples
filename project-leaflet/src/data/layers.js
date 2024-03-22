900fe0e0b325e9c02e87cea438347bcdc4a6beec
einerzg <einerzg@gmail.com>
2024-03-22 15:17:16 -0500

    Improve pratices

--- project-leaflet/src/data/layers.js
+++ project-leaflet/src/data/layers.js
@@ -5,9 +5,9 @@ const ATRIBUTIONS_LIST = {
   CyclOSM:
     '<a href="https://github.com/cyclosm/cyclosm-cartocss-style/releases" title="CyclOSM - Open Bicycle render">CyclOSM</a> | Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
   EsriWorldImagery:
-    "Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community",
+    'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community',
   EsriWorldTopoMap:
-    "Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ, TomTom, Intermap, iPC, USGS, FAO, NPS, NRCAN, GeoBase, Kadaster NL, Ordnance Survey, Esri Japan, METI, Esri China (Hong Kong), and the GIS User Community",
+    'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ, TomTom, Intermap, iPC, USGS, FAO, NPS, NRCAN, GeoBase, Kadaster NL, Ordnance Survey, Esri Japan, METI, Esri China (Hong Kong), and the GIS User Community',
   OSM: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
   OSMHot:
     '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Tiles style by <a href="https://www.hotosm.org/" target="_blank">Humanitarian OpenStreetMap Team</a> hosted by <a href="https://openstreetmap.fr/" target="_blank">OpenStreetMap France</a>',
@@ -17,17 +17,17 @@ const ATRIBUTIONS_LIST = {
     '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors',
   ThunderForest:
     '&copy; <a href="http://www.thunderforest.com/">Thunderforest</a>, &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-  USGS: 'Tiles courtesy of the <a href="https://usgs.gov/">U.S. Geological Survey</a>',
+  USGS: 'Tiles courtesy of the <a href="https://usgs.gov/">U.S. Geological Survey</a>'
 };
 
-const thunderForestKey = "apikey=883b04efb7454e2da35b028c800fa3a6";
+const thunderForestKey = 'apikey=883b04efb7454e2da35b028c800fa3a6';
 export const layers = {
   baseLayers: {
     default: {
-      map: "https://tile.openstreetmap.org/{z}/{x}/{y}.png",
-      atribution: ATRIBUTIONS_LIST.OSM,
+      map: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+      atribution: ATRIBUTIONS_LIST.OSM
     },
-    blackWhite: "http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png",
+    blackWhite: 'http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png',
     thunderForest: {
       map: {
         openCycleMap: `https://tile.thunderforest.com/cycle/{z}/{x}/{y}.png?${thunderForestKey}`,
@@ -39,89 +39,89 @@ export const layers = {
         pioneer: `https://tile.thunderforest.com/pioneer/{z}/{x}/{y}.png?${thunderForestKey}`,
         mobileAtlas: `https://tile.thunderforest.com/mobile-atlas/{z}/{x}/{y}.png?${thunderForestKey}`,
         neighbourhood: `https://tile.thunderforest.com/neighbourhood/{z}/{x}/{y}.png?${thunderForestKey}`,
-        atlas: `https://tile.thunderforest.com/atlas/{z}/{x}/{y}.png?${thunderForestKey}`,
+        atlas: `https://tile.thunderforest.com/atlas/{z}/{x}/{y}.png?${thunderForestKey}`
       },
-      atribution: ATRIBUTIONS_LIST.ThunderForest,
+      atribution: ATRIBUTIONS_LIST.ThunderForest
     },
     osmManik: {
-      map: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
-      atribution: ATRIBUTIONS_LIST.OSM,
+      map: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+      atribution: ATRIBUTIONS_LIST.OSM
     },
     osmDE: {
-      map: "https://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png",
-      atribution: ATRIBUTIONS_LIST.OSM,
+      map: 'https://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png',
+      atribution: ATRIBUTIONS_LIST.OSM
     },
     osmHot: {
-      map: "http://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png",
-      atribution: ATRIBUTIONS_LIST.OSMHot,
+      map: 'http://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png',
+      atribution: ATRIBUTIONS_LIST.OSMHot
     },
     openTopoMap: {
-      map: "https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png",
-      atribution: ATRIBUTIONS_LIST.OpenTopo,
+      map: 'https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png',
+      atribution: ATRIBUTIONS_LIST.OpenTopo
     },
     stadia: {
       map: {
         AlidadeSmooth:
-          "https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png",
+          'https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png',
         AlidadeSmoothDark:
-          "https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png",
+          'https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png',
         OsmBright:
-          "https://tiles.stadiamaps.com/tiles/osm_bright/{z}/{x}/{y}{r}.png",
+          'https://tiles.stadiamaps.com/tiles/osm_bright/{z}/{x}/{y}{r}.png',
         Outdoors:
-          "https://tiles.stadiamaps.com/tiles/outdoors/{z}/{x}/{y}{r}.png",
+          'https://tiles.stadiamaps.com/tiles/outdoors/{z}/{x}/{y}{r}.png'
       },
-      atribution: ATRIBUTIONS_LIST.Stadia,
+      atribution: ATRIBUTIONS_LIST.Stadia
     },
     cycloOsm: {
-      map: "https://{s}.tile-cyclosm.openstreetmap.fr/cyclosm/{z}/{x}/{y}.png",
-      atribution: ATRIBUTIONS_LIST.CyclOSM,
+      map: 'https://{s}.tile-cyclosm.openstreetmap.fr/cyclosm/{z}/{x}/{y}.png',
+      atribution: ATRIBUTIONS_LIST.CyclOSM
     },
     esri: {
       worldStreetMap: {
-        map: "https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}",
-        atribution: ATRIBUTIONS_LIST.EsriWorldTopoMap,
+        map: 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}',
+        atribution: ATRIBUTIONS_LIST.EsriWorldTopoMap
       },
       worldImagery: {
-        map: "https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}",
-        atribution: ATRIBUTIONS_LIST.EsriWorldImagery,
-      },
+        map: 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
+        atribution: ATRIBUTIONS_LIST.EsriWorldImagery
+      }
     },
     cartoDb: {
       map: {
         positron:
-          "https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png",
+          'https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png',
         positronNoLabels:
-          "https://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}{r}.png",
+          'https://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}{r}.png',
         voyager:
-          "https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png",
+          'https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png'
       },
-      atribution: ATRIBUTIONS_LIST.CartoDb,
+      atribution: ATRIBUTIONS_LIST.CartoDb
     },
     usgsUs: {
       map: {
-        topo: "https://basemap.nationalmap.gov/arcgis/rest/services/USGSTopo/MapServer/tile/{z}/{y}/{x}",
+        topo: 'https://basemap.nationalmap.gov/arcgis/rest/services/USGSTopo/MapServer/tile/{z}/{y}/{x}',
         imagery:
-          "https://basemap.nationalmap.gov/arcgis/rest/services/USGSImageryOnly/MapServer/tile/{z}/{y}/{x}",
+          'https://basemap.nationalmap.gov/arcgis/rest/services/USGSImageryOnly/MapServer/tile/{z}/{y}/{x}'
       },
-      atribution: ATRIBUTIONS_LIST.USGS,
+      atribution: ATRIBUTIONS_LIST.USGS
     },
     hikeBike: {
-      map: "https://tiles.wmflabs.org/hikebike/{z}/{x}/{y}.png",
-      atribtuion: ATRIBUTIONS_LIST.OSM,
-    },
+      map: 'https://tiles.wmflabs.org/hikebike/{z}/{x}/{y}.png',
+      atribtuion: ATRIBUTIONS_LIST.OSM
+    }
   },
   overlayers: {
-    openSeaMap: "https://tiles.openseamap.org/seamark/{z}/{x}/{y}.png",
-    openPtMap: "http://openptmap.org/tiles/{z}/{x}/{y}.png",
+    openSeaMap: 'https://tiles.openseamap.org/seamark/{z}/{x}/{y}.png',
+    openPtMap: 'http://openptmap.org/tiles/{z}/{x}/{y}.png',
     openRailway:
-      "https://{s}.tiles.openrailwaymap.org/standard/{z}/{x}/{y}.png",
+      'https://{s}.tiles.openrailwaymap.org/standard/{z}/{x}/{y}.png',
     wayMarkedTrails: {
-      hiking: "https://tile.waymarkedtrails.org/hiking/{z}/{x}/{y}.png",
-      cycling: "https://tile.waymarkedtrails.org/cycling/{z}/{x}/{y}.png",
-      mtb: "https://tile.waymarkedtrails.org/mtb/{z}/{x}/{y}.png",
-      slopes: "https://tile.waymarkedtrails.org/slopes/{z}/{x}/{y}.png",
+      hiking: 'https://tile.waymarkedtrails.org/hiking/{z}/{x}/{y}.png',
+      cycling: 'https://tile.waymarkedtrails.org/cycling/{z}/{x}/{y}.png',
+      mtb: 'https://tile.waymarkedtrails.org/mtb/{z}/{x}/{y}.png',
+      slopes: 'https://tile.waymarkedtrails.org/slopes/{z}/{x}/{y}.png'
     },
-    openSnowMap: "https://tiles.opensnowmap.org/pistes/{z}/{x}/{y}.png",
+    openSnowMap: 'https://tiles.opensnowmap.org/pistes/{z}/{x}/{y}.png'
   }
 };
 
@@ -137,6 +137,6 @@ export const layersWMS = {
       srtmThirtyHillshade: 'SRTM30-Hillshade',
       srtmThirtyColored: 'SRTM30-Colored',
       srtmThirtyColoredHillshade: 'SRTM30-Colored-Hillshade'
-    },
-  },
+    }
+  }
 };
--- project-leaflet/src/helper/index.js
+++ project-leaflet/src/helper/index.js
@@ -1,22 +1,23 @@
-import { tileLayer } from "leaflet";
-import { layers, layersWMS } from "../data/layers";
-import { MAX_ZOOM, MIN_ZOOM } from "./constants";
+/* eslint-disable space-before-function-paren */
+import { tileLayer } from 'leaflet';
+import { layers, layersWMS } from '../data/layers';
+import { MAX_ZOOM, MIN_ZOOM } from './constants';
 
 // Const for basic tile layer
 const DEFAULT_LAYER = layers.baseLayers.default;
 const DEFAULT_OPTIONS = {
   minZoom: MIN_ZOOM,
   maxZoom: MAX_ZOOM,
-  attribution: DEFAULT_LAYER.atribution,
+  attribution: DEFAULT_LAYER.atribution
 };
 
 // Const for tile layer with wms
 const DEFAULT_LAYER_WMS = layersWMS.mundialis;
 const DEFAULT_OPTIONS_WMS = {
   layers: DEFAULT_LAYER_WMS.layers.osmOverlayWMS,
-  format: "image/png",
+  format: 'image/png',
   transparent: true,
-  ...DEFAULT_OPTIONS,
+  ...DEFAULT_OPTIONS
 };
 
 export function createTileLayers(
